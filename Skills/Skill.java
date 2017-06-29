646478309ef1ff05e70ae73174b0ddfdf0f4db7f
sumwinds <sumwinds@163.com>
2017-06-29 21:12:51 +0800

    对技能项进行修改

--- Skills/Skill.java
+++ Skills/Skill.java
@@ -1,11 +1,40 @@
 package Skills;
 
-public interface Skill {
-	int CD = 0;
-	int ATK = 0;
-	String description = null;
-	String effect = null;
+import Monster.Monsters;
+import magicCastle.Player;
+
+public class Skill {
+	private int CD = 0;
+	private int f_CD = 0;
+	private int ATK = 0;
+	private String description = null;
+	private String d_effect = null;
+	private boolean used = false;
 	
-	public void setInformation(int CD, int ATK, String description, String effect);
-	public boolean isCD();
+	public Skill(int CD, int ATK, String description, String effect) {
+		this.CD = CD;
+		this.f_CD = CD;
+		this.ATK = ATK;
+		this.description = description;
+		this.d_effect = effect;
+	}
+	
+	public boolean isCD() {
+		if(CD == f_CD) {
+			return true;
+		}
+		return false;
+	}
+	
+	public void learn() {
+		used = true;
+	}
+	
+	public void effect(Monsters monster, Player player) {
+		
+	}
+	
+	public int getATK() {
+		return ATK;
+	}
 }
